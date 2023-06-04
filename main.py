c9549b876b39fe6a51780f0706828d4b1e641330
Seredinbis <78419583+Seredinbis@users.noreply.github.com>
2023-06-04 15:50:34 +0300

    add host

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-webh_data = None
+webh_data = Noneq
 
 @app.route('/', methods=['POST', 'GET', 'PUT'])
 def webhook():
@@ -17,4 +17,4 @@ def webhook():
     return '200'  # отправляем ответ серверу, чтобы он знал, что запрос успешно обработан
 
 
-app.run(debug=True, port=5000)  # запускаем приложение в режиме отладки
+app.run(host='0.0.0.0', debug=True, port=5000)  # запускаем приложение в режиме отладки
