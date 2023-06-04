89d4673c5afaa5a2ba2740c107cc1ea04ba771d1
Seredinbis <78419583+Seredinbis@users.noreply.github.com>
2023-06-04 13:02:00 +0300

    test time

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import asyncio
+
+app = Flask(__name__)
+
+webh_data = None
+
+@app.route('/', methods=['POST', 'GET', 'PUT'])
+def webhook():
+    # data = request.get_json()  # получаем данные из запроса в формате JSON
+    global webh_data
+    webh_data = request.json
+    print(webh_data)  # выводим полученные данные в консоль
+    print()
+    print(webh_data['object']['source'])
+    print(webh_data['object']['source']['pay'])
+    print(webh_data['object']['source']['pay']['first'])
+    return '200'  # отправляем ответ серверу, чтобы он знал, что запрос успешно обработан
+git add main.oy
+
+app.run(debug=True, port=5000)  # запускаем приложение в режиме отладки
